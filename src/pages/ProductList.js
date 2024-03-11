0dc87350bec1d99e44b154c84d61645d7bef2efc
dara-coder <darasmart2020@gmail.com>
2024-03-11 21:09:14 +0700

    update ui

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,5 +1,45 @@
 import { Fragment } from 'react';
+import DataTable from 'react-data-table-component';
 import { useNavigate } from 'react-router-dom';
+
+const columns = [
+    {
+        name: 'Code',
+        selector: row => row.code
+    },
+    {
+        name: 'Name',
+        selector: row => row.name
+    },
+    {
+        name: 'Category',
+        selector: row => row.category
+    },
+    {
+        name: 'Price',
+        selector: row => row.price
+    }
+];
+
+const data = [
+    {
+        id: 1,
+        code: 'PR-0001',
+        name: 'Product',
+        category: 'Category',
+        price: 100
+    }
+];
+
+const customStyles = {
+    headCells: {
+        style: {
+            fontSize: '1rem',
+            fontWeight: 'bold',
+            color: '#0d6efd'
+        }
+    }
+};
 
 function ProductList()
 {
@@ -17,7 +57,8 @@ function ProductList()
                         className="form-control"
                         placeholder="search by name of product"/>
                     <span
-                        className="input-group-text" role="button">
+                        className="input-group-text"
+                        role="button">
                         <i
                             className="fa-solid fa-magnifying-glass"></i>
                     </span>
@@ -31,7 +72,13 @@ function ProductList()
                 </button>
             </div>
             <div
-                className="bg-white rounded-3 p-3 border mt-3 animate__animated animate__fadeInRight"></div>
+                className="bg-white rounded-3 p-3 border mt-3 animate__animated animate__fadeInRight">
+                <DataTable
+                    pagination
+                    columns={columns}
+                    data={data}
+                    customStyles={customStyles}/>
+            </div>
         </Fragment>
     );
 }
